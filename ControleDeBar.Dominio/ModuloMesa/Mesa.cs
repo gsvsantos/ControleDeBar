e096e09b75db76ecc2751df8b33ea3838ed62692
gsvsantos <gustavocnsantos02@gmail.com>
2025-06-10 01:21:39 -0300

    feat: Implementa ModuloMesa em Dominio.

---
+++ ControleDeBar.Dominio/ModuloMesa/Mesa.cs
@@ -0,0 +1,23 @@
+﻿using ControleDeBar.Dominio.Compartilhado;
+
+namespace ControleDeBar.Dominio.ModuloMesa;
+
+public class Mesa : EntidadeBase<Mesa>
+{
+    public int Numero { get; set; }
+    public int Capacidade { get; set; }
+
+    public Mesa() { }
+
+    public Mesa(int numero, int capacidade) : this()
+    {
+        Numero = numero;
+        Capacidade = capacidade;
+    }
+
+    public override void AtualizarRegistro(Mesa registroEditado)
+    {
+        Numero = registroEditado.Numero;
+        Capacidade = registroEditado.Numero;
+    }
+}
