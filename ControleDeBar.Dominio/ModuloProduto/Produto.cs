ba12209e1f177c3a8983a62386a85909a8dc269f
gsvsantos <gustavocnsantos02@gmail.com>
2025-06-12 15:40:17 -0300

    feat: Implementa ModuloProduto em Dominio.

---
+++ ControleDeBar.Dominio/ModuloProduto/Produto.cs
@@ -0,0 +1,22 @@
+﻿using ControleDeBar.Dominio.Compartilhado;
+
+namespace ControleDeBar.Dominio.ModuloProduto;
+
+public class Produto : EntidadeBase<Produto>
+{
+    public string Nome { get; set; } = string.Empty;
+    public decimal Preco { get; set; }
+
+    public Produto() { }
+    public Produto(string nome, decimal preco)
+    {
+        Nome = nome;
+        Preco = preco;
+    }
+
+    public override void AtualizarRegistro(Produto registroEditado)
+    {
+        Nome = registroEditado.Nome;
+        Preco = registroEditado.Preco;
+    }
+}
