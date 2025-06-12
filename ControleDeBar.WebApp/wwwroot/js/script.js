3992d27e45cf01736139d147ad29a5746ea0341d
gsvsantos <gustavocnsantos02@gmail.com>
2025-06-12 20:08:03 -0300

    refactor: Melhora visual e funcional do _Layout e _SideBar.

--- ControleDeBar.WebApp/wwwroot/js/script.js
+++ ControleDeBar.WebApp/wwwroot/js/script.js
@@ -1,10 +1,17 @@
 const hamburguer = document.querySelector('.toggle-btn');
 const toggler = document.querySelector('.toggle-btn-icon');
+var x = window.matchMedia("(max-width: 992px)")
 
 function sidebarToggler() {
     document.querySelector('.sidebar').classList.toggle('expand');
-    toggler.classList.toggle('bi-chevron-double-right');
     toggler.classList.toggle('bi-chevron-double-left');
+    toggler.classList.toggle('bi-chevron-double-right');
 }
 
 hamburguer.addEventListener('click', sidebarToggler);
+
+sidebarToggler(x);
+
+x.addEventListener("change", function () {
+    sidebarToggler(x);
+});
