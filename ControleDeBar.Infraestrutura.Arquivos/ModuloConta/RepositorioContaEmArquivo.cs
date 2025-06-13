ad64eabb81164e607472031a5c75879e89792b67
gsvsantos <gustavocnsantos02@gmail.com>
2025-06-12 22:22:26 -0300

    feat: Implementa ModuloConta em Infraestrutura.Arquivos.

---
+++ ControleDeBar.Infraestrutura.Arquivos/ModuloConta/RepositorioContaEmArquivo.cs
@@ -0,0 +1,64 @@
+﻿using ControleDeBar.Dominio.ModuloConta;
+using ControleDeBar.Infraestrutura.Arquivos.Compartilhado;
+
+namespace ControleDeBar.Infraestrutura.Arquivos.ModuloConta;
+
+public class RepositorioContaEmArquivo : RepositorioBaseEmArquivo<Conta>, IRepositorioConta
+{
+    public RepositorioContaEmArquivo(ContextoDados contexto) : base(contexto) { }
+
+    public void CadastrarConta(Conta novaConta)
+    {
+        registros.Add(novaConta);
+
+        contexto.Salvar();
+    }
+
+    public Conta SelecionarPorId(Guid idRegistro)
+    {
+        foreach (Conta c in registros)
+        {
+            if (c.Id == idRegistro)
+                return c;
+        }
+
+        return null!;
+    }
+
+
+    public List<Conta> SelecionarContas()
+    {
+        return registros;
+    }
+
+    public List<Conta> SelecionarContasAbertas()
+    {
+        List<Conta> contasAbertas = [];
+
+        foreach (Conta c in registros)
+        {
+            if (c.EstaAberta)
+                contasAbertas.Add(c);
+        }
+
+        return contasAbertas;
+    }
+
+    public List<Conta> SelecionarContasFechadas()
+    {
+        List<Conta> contasFechadas = [];
+
+        foreach (Conta c in registros)
+        {
+            if (!c.EstaAberta)
+                contasFechadas.Add(c);
+        }
+
+        return contasFechadas;
+    }
+
+    protected override List<Conta> ObterRegistros()
+    {
+        return contexto.Contas;
+    }
+}
