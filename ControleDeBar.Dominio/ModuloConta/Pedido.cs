42190993074e678d550834eff87a1f01a321e0ba
gsvsantos <gustavocnsantos02@gmail.com>
2025-06-12 22:21:57 -0300

    feat: Implementa ModuloConta em Dominio.

---
+++ ControleDeBar.Dominio/ModuloConta/Pedido.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using ControleDeBar.Dominio.ModuloProduto;
+
+namespace ControleDeBar.Dominio.ModuloConta;
+
+public class Pedido
+{
+    public Guid Id { get; set; }
+    public Produto Produto { get; set; }
+    public int QuantidadeSolicitada { get; set; }
+
+    [ExcludeFromCodeCoverage]
+    public Pedido() { }
+    public Pedido(Produto produto, int quantidadeSolicitada) : this()
+    {
+        Id = Guid.NewGuid();
+        Produto = produto;
+        QuantidadeSolicitada = quantidadeSolicitada;
+    }
+
+    public decimal CalcularTotalParcial()
+    {
+        return Produto.Preco * QuantidadeSolicitada;
+    }
+
+    public override string ToString()
+    {
+        return $"{QuantidadeSolicitada}x {Produto.Preco}";
+    }
+}
