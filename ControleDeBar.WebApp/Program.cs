495e5b1121b1ee22e80f44fbcbeffbe9d7280107
gsvsantos <gustavocnsantos02@gmail.com>
2025-06-30 20:50:47 -0300

    refactor: Implementa configurações faltantes para uso de Serilog, ExceptionHandler(/erro) e validações.

--- ControleDeBar.WebApp/Program.cs
+++ ControleDeBar.WebApp/Program.cs
@@ -1,4 +1,7 @@
 using ControleDeBar.WebApp.ActionFilters;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
 
 namespace ControleDeBar.WebApp
 {
@@ -15,7 +18,30 @@ namespace ControleDeBar.WebApp
                 options.Filters.Add<LogarAcaoAttribute>();
             });
 
-            var app = builder.Build();
+            string caminhoAppData = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+            string caminhoArquivo = Path.Combine(caminhoAppData, "eAgenda", "erro.log");
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(new CompactJsonFormatter(), caminhoArquivo, LogEventLevel.Error)
+                .CreateLogger();
+
+            builder.Logging.ClearProviders();
+
+            builder.Services.AddSerilog(Log.Logger);
+
+            WebApplication app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+                app.UseExceptionHandler("/erro");
+            else
+                app.UseDeveloperExceptionPage();
+
+            app.UseAntiforgery();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
             app.MapDefaultControllerRoute();
